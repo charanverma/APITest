cd6dc2c4e2b64f7d3205b50c3a6ca7f29da3ce7f
Charan Singh <charanverma@outlook.com>
2020-01-18 19:39:24 -0500

    test project added

---
+++ ApiUnitTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+namespace ApiUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+
+
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(2,2);
+
+
+        }
+    }
+}
